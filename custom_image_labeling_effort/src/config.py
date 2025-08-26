aa2cd3806324ba2b2fbb914f80b920f12292b865
Hasnat79 <hasnatabdullah79@gmail.com>
2025-08-26 16:16:14 -0500

    config update

--- custom_image_labeling_effort/src/config.py
+++ custom_image_labeling_effort/src/config.py
@@ -1,2 +1,11 @@
 UNLABELED_DATAPATH = "/data/hma18/ally_hasnat/data/unlabeled"
-COLOR_MAP_IMG_OUTPUT_DIR = "color_mapped_images"
+COLOR_MAP_IMG_OUTPUT_DIR = "/data/hma18/ally_hasnat/custom_image_labeling_effort/data/unlabled_color_mapped_images"
+RBBOX_LABELED_DATAPATH = "../data/rbbox_labels_for_color_mapped_images"
+
+PREVIEW_DIR = "/data/hma18/ally_hasnat/custom_image_labeling_effort/outputs/previews"
+
+CROPPED_DIR = "/data/hma18/ally_hasnat/custom_image_labeling_effort/outputs/cropped"
+# ----------------
+# cattle obb
+LABELED_COLOR_MAPPED_IMAGES_DIR = "/data/hma18/ally_hasnat/custom_image_labeling_effort/data/labeled_color_mapped_images"
+LABELS_XML_DIR = "/data/hma18/ally_hasnat/custom_image_labeling_effort/data/rbbox_labels_for_color_mapped_images"
